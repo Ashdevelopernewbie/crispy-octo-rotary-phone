fdbf22c76e2f66ca28944384ccc86248adaf004b
Ashbel lama <aahashish100@gmail.com>
2022-09-26 22:09:06 +0545

    Lesson on how strings can be written.

---
+++ formatted_strings.py
@@ -0,0 +1,12 @@
+first_name = 'John'
+last_name = 'Smith'
+
+#string concatenation
+message = first_name + ' [' + last_name + ']' ' is a coder'
+print(message)
+
+#formatted strings
+msg = f'{first_name} [{last_name}] is a coder'
+print(msg)
+
+
